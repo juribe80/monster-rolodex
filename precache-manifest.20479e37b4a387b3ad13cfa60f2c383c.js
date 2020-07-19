e5363301b7ed189b2d00e56d5e634850aa059379
Juan Uribe <juanuribe80@gmail.com>
2020-07-18 23:22:30 -0400

    Updates

---
+++ precache-manifest.20479e37b4a387b3ad13cfa60f2c383c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6946f0e2467da49743c75d1099193052",
+    "url": "/juribe80/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c911a1438908d46346d3",
+    "url": "/juribe80/monster-rolodex/static/css/main.988b711e.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/juribe80/monster-rolodex/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/juribe80/monster-rolodex/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c911a1438908d46346d3",
+    "url": "/juribe80/monster-rolodex/static/js/main.8d18c7c7.chunk.js"
+  },
+  {
+    "revision": "3f7140b93e98dc1dd461",
+    "url": "/juribe80/monster-rolodex/static/js/runtime-main.e2e09e46.js"
+  }
+]);
